33cc384cf436bc380165b9096790d6c9e7c7aef4
Kevin <kevin.ou705@gmail.com>
2018-09-04 19:00:36 -0700

    Work on project

--- Kevin_Ou_ap2/lib/p02_hashing.rb
+++ Kevin_Ou_ap2/lib/p02_hashing.rb
@@ -1,14 +1,22 @@
+require 'byebug'
+
 class Fixnum
   # Fixnum#hash already implemented for you
 end
 
 class Array
   def hash
+    sum = 0
+    self.each_with_index {|el, i| sum += el.hash * i }
+    sum
   end
 end
 
 class String
   def hash
+    sum = 0
+    self.each_char.with_index {|el, i| sum += el.ord * (i + 1) }
+    sum
   end
 end
 
@@ -16,6 +24,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    sum = 0
+    i = 5
+    self.each do |k, v|
+      # debugger
+      sum += k.to_s.hash * v.hash
+      i += 5
+    end
+    sum
   end
 end
--- Kevin_Ou_ap2/lib/p03_hash_set.rb
+++ Kevin_Ou_ap2/lib/p03_hash_set.rb
@@ -1,4 +1,5 @@
 require_relative 'p02_hashing'
+require 'byebug'
 
 class HashSet
   attr_reader :count
@@ -9,18 +10,27 @@ class HashSet
   end
 
   def insert(key)
+    resize! if num_buckets == count
+    self[key.hash].push(key)
+    @count += 1
+    true
   end
-
+  
   def include?(key)
+    self[key.hash].include?(key)
   end
-
+  
   def remove(key)
+    self[key.hash].delete(key)
+    @count -= 1
+    true
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +38,8 @@ class HashSet
   end
 
   def resize!
+    new_buckets = Array.new(num_buckets * 2) { Array.new }
+    @store.flatten.each { |el| new_buckets[el.hash % new_buckets.length].push(el)}
+    @store = new_buckets
   end
 end
