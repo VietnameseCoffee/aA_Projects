c6ee1bec79968fc1d83607838f49a4b3ebb0f198
Kevin <kevin.ou705@gmail.com>
2018-09-05 00:24:15 -0700

    Finish most of the project

--- Kevin_Ou_ap2/lib/p04_linked_list.rb
+++ Kevin_Ou_ap2/lib/p04_linked_list.rb
@@ -1,25 +1,38 @@
 class Node
+  
   attr_accessor :key, :val, :next, :prev
-
+  
   def initialize(key = nil, val = nil)
     @key = key
     @val = val
     @next = nil
     @prev = nil
   end
-
+  
   def to_s
     "#{@key}: #{@val}"
   end
-
+  
   def remove
     # optional but useful, connects previous node to next node
     # and removes self from list.
+    prev = self.prev
+    after = self.next
+    
+    prev.next = after
+    after.prev = prev
+    self.prev = nil
+    self.next = nil
   end
 end
 
 class LinkedList
+  include Enumerable
   def initialize
+    @head = Node.new("head", "head")
+    @tail = Node.new("tail", "tail")
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,30 +41,69 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each {|node| return node.val if key == node.key}
+    nil
   end
 
   def include?(key)
+    self.each {|node| return true if node.key == key }
+    false
   end
 
   def append(key, val)
+    before = @tail.prev
+    node = Node.new(key, val)
+
+    @tail.prev = node
+    before.next = node
+    node.prev = before
+    node.next = @tail
+    
   end
 
   def update(key, val)
-  end
 
+    self.each do |node|
+      if node.key == key
+        node.val = val
+        return true
+      end
+    end
+    nil
+  end
+  
   def remove(key)
+
+    self.each do |node|
+      if node.key == key
+        node.remove
+        return true
+      end
+    end
+    nil
+
   end
 
-  def each
+  def each(&prc)
+    # extend Enumerable
+    current_node = @head.next
+
+    while current_node != @tail
+      prc.call(current_node)
+      current_node = current_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
--- Kevin_Ou_ap2/lib/p05_hash_map.rb
+++ Kevin_Ou_ap2/lib/p05_hash_map.rb
@@ -2,6 +2,7 @@ require_relative 'p02_hashing'
 require_relative 'p04_linked_list'
 
 class HashMap
+  include Enumerable
   attr_reader :count
 
   def initialize(num_buckets = 8)
@@ -10,18 +11,35 @@ class HashMap
   end
 
   def include?(key)
+    # @store[key.hash % num_buckets].include?(key)
+    self.each {|k, v| return true if k == key}
+    return false
   end
 
   def set(key, val)
+    resize! if @count == num_buckets
+    list = @store[key.hash % num_buckets]
+    if  include?(key)
+      list.update(key, val)
+    else 
+      list.append(key, val)
+      @count += 1
+    end
+    true
   end
 
   def get(key)
+    @store[key.hash % num_buckets].get(key)
   end
 
   def delete(key)
+    @count -= 1 if @store[key.hash % num_buckets].remove(key)
   end
 
-  def each
+  def each(&prc)
+    @store.each do |bucket|
+      bucket.each {|node| prc.call(node.key, node.val)}
+    end
   end
 
   # uncomment when you have Enumerable included
@@ -42,6 +60,10 @@ class HashMap
   end
 
   def resize!
+    new_hash = Array.new(num_buckets * 2) { LinkedList.new }
+
+    @store.each {|k, v| new_hash[(k.hash) % (num_buckets * 2)].append(k, v)}
+    @store = new_hash
   end
 
   def bucket(key)
