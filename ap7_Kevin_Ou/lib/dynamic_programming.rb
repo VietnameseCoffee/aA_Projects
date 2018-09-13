101dcd0c234c0e713e0ce27d664fd12151a9277f
Kevin <kevin.ou705@gmail.com>
2018-09-13 00:48:26 -0700

    Work on algorithms

--- ap7_Kevin_Ou/lib/dynamic_programming.rb
+++ ap7_Kevin_Ou/lib/dynamic_programming.rb
@@ -57,16 +57,61 @@ class DynamicProgramming
   end
 
   def super_frog_hops(n, k)
+    return [[]] if n == 0
+    return [[1]] if n == 1
 
+    results = []
+
+    (1..k).each do |i|
+      break if i > n
+
+      last = i == n ? [n] : super_frog_hops(i, k)[-1]
+      next_jump = super_frog_hops(n-i, k)
+
+      last.each do |el1|
+        next_jump.each do |el2|
+          results.push([el1] + el2)
+        end
+      end
+    end
+
+  	results
   end
 
   def knapsack(weights, values, capacity)
-
+    # @cache ||= 
+    # debugger if capacity == 165
+    knapsack_table(weights, values, capacity)
+    # debugger
+    return @cache.last.max
   end
 
   # Helper method for bottom-up implementation
   def knapsack_table(weights, values, capacity)
+    return @cache = [Array.new(weights.length,0)] if capacity.zero?
 
+    knapsack_table(weights, values, capacity -1) if capacity > 0
+
+    cache.push(Array.new(weights.length,0))
+
+    # if capacity == 25
+    # debugger
+    # end
+    weights.each_index do |i|
+      if i == 0
+        if weights[i] <= capacity
+          cache.last[i] = values[i]
+        end
+      else
+        dif = capacity - weights[i] 
+        if dif < 0
+          cache.last[i] = cache.last.max
+        else
+          new_sum = values[i] + cache[dif][i - 1]
+          cache.last[i] = [new_sum, cache.last.max].max
+        end
+      end
+    end
   end
 
   def maze_solver(maze, start_pos, end_pos)
@@ -74,3 +119,9 @@ class DynamicProgramming
 
   attr_reader :cache
 end
+
+
+# display capacity
+# display cache
+# display cache.last
+# display i
